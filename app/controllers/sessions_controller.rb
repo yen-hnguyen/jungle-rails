2b75882c332bbcc2b8ceea21689ad014ce77be73
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-04-18 02:43:30 +0000

    add new routes and update session logic to log users in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,25 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      #Save the user id inside brower cookie.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else    
+      #If user's login doesn't work, send them back to the login form
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 
 end
